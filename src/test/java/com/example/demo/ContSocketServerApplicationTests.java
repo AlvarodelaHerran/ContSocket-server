e067ace5b5d4cb028f3d673d02bb2d292dbd16a3
AlvarodelaHerran <alvarodela.herran@opendeusto.es>
2025-11-21 10:38:32 +0100

    Desesperación

---
+++ src/test/java/com/example/demo/ContSocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContSocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
